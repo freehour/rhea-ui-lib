401c7d3a949f07d6a9a40cfc34d5b6ca54a8b1a3
freehour <ka1@posteo.de>
2026-03-20 14:35:11 +0000

    select-item now includes indicator

--- components/select/index.ts
+++ components/select/index.ts
@@ -1,6 +1,5 @@
 export * from './select-content';
 export * from './select-group';
-export * from './select-item-indicator';
 export * from './select-item';
 export * from './select-label';
 export * from './select-scroll-down-button';
