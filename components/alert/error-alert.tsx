d24cc298660e2538fc27fe9d00a42a91fc644024
freehour <ka1@posteo.de>
2026-04-22 07:44:01 +0000

    error alert component

---
+++ components/alert/error-alert.tsx
@@ -0,0 +1,71 @@
+import { AlertCircleIcon, ChevronLeftIcon } from 'lucide-react';
+import type { FunctionComponent } from 'react';
+
+import { Button } from '@/components/button';
+import type { CollapsibleProps } from '@/components/collapsible';
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/collapsible';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/tooltip';
+import { cn } from '@/utils';
+
+import { Alert } from './alert';
+import { AlertAction } from './alert-action';
+import { AlertDescription } from './alert-description';
+import { AlertTitle } from './alert-title';
+
+
+export interface ErrorAlertProps extends CollapsibleProps {
+    error: Error;
+}
+
+export const ErrorAlert: FunctionComponent<ErrorAlertProps> = ({
+    error: {
+        name,
+        message,
+        cause,
+    },
+    className,
+    ...props
+}) => (
+    <Collapsible
+        className={cn('rounded-sm bg-destructive/10', className)}
+        {...props}
+    >
+        <Alert
+            variant="destructive"
+            className="border-none bg-transparent"
+        >
+            <AlertCircleIcon />
+            <AlertTitle>{name}</AlertTitle>
+            {cause !== undefined && (
+                <AlertAction>
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <CollapsibleTrigger>
+                                <Button
+                                    size="icon-xs"
+                                    variant="secondary"
+                                >
+                                    <ChevronLeftIcon className="transition-transform group-data-[state=open]/button:-rotate-90" />
+                                </Button>
+                            </CollapsibleTrigger>
+                        </TooltipTrigger>
+                        <TooltipContent side="right">
+                            Cause
+                        </TooltipContent>
+                    </Tooltip>
+                </AlertAction>
+            )}
+            <AlertDescription className="text-destructive/90">
+                {message}
+            </AlertDescription>
+        </Alert>
+        <CollapsibleContent className="pl-8.5">
+            {cause instanceof Error && (
+                <ErrorAlert
+                    error={cause}
+                    className="rounded-tr-none rounded-bl-none"
+                />
+            )}
+        </CollapsibleContent>
+    </Collapsible>
+);
--- components/alert/index.ts
+++ components/alert/index.ts
@@ -2,3 +2,4 @@ export * from './alert';
 export * from './alert-action';
 export * from './alert-description';
 export * from './alert-title';
+export * from './error-alert';
